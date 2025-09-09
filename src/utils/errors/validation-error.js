ad59faf61c5cd74bc1898e09b218c06ef829d527
Harsh Kumar <harshbardhan7546@gmail.com>
2025-09-09 06:41:58 +0530

    Added error file

---
+++ src/utils/errors/validation-error.js
@@ -0,0 +1,18 @@
+const {StatusCodes} = require('http-status-codes') 
+
+class ValidationError extends Error{
+    constructor(error){
+        super();
+        let explaination =[];
+        error.errors.array.forEach(err => {
+            explaination.push(err.message)
+        });
+
+            this.name = 'ValidationError'
+            this.message = "Not able to validate the data"
+            this.explaination = explaination
+            this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
+module.exports = ValidationError;
