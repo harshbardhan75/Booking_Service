bda4c4d2b7cece8baf7ec460d099ff77be928382
Harsh Kumar <harshbardhan7546@gmail.com>
2025-09-09 20:13:58 +0530

    Added create in repository

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,27 @@
+
+const {Booking} = require('../models/index');
+const { AppError,ValidationError } = require('../utils/errors/index');
+const{StatusCodes} = require('http-status-codes')
+
+
+class BookingRepository{
+
+    async create(data){
+        try {
+            const booking = await Booking.create(data);
+            return booking;
+            
+        } catch (error) {
+            if(error.name == 'SequelizeValidationError' ){
+                throw new ValidationError(error);
+            }
+            throw new AppError('Repository Error','Cannot createBooking','There was some issue creating the booking',StatusCodes.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    async update(data){
+        
+    }
+
+}
+module.exports = BookingRepository;
